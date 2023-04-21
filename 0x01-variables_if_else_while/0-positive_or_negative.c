eb6959b2f5309fc583c0c3581f28eebc0c71abd1
manal <manal.shissah@icloud.com>
2023-04-21 13:15:13 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,24 +5,19 @@
 #include <time.h>
 
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 
 {
 
-int n;
+	int n;
 
-srand(time(0));
+	srand(time(0));
 
-n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 /* your code goes there */
 
