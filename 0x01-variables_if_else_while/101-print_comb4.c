982925154e088e224c9044eb93d47122a48a6293
manal <manal.shissah@icloud.com>
2023-04-21 16:01:21 -0700

    asmaa

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,29 +5,20 @@
 * main - prints all possible different combinations of three digits
 * Return: Always 0 (Success)
 */
-
 int main(void)
-
 {
-
 	int n, m, l;
 
 	for (n = 48; n < 58; n++)
-
 	{
-
 		for (m = 49; m < 58; m++)
-
 		{
-
 			for (l = 50; l < 58; l++)
 
 			{
-
 				if (l > m && m > n)
 
 				{
-
 					putchar(n);
 
 					putchar(m);
@@ -37,20 +28,13 @@ int main(void)
 					if (n != 55 || m != 56)
 
 					{
-
 						putchar(',');
 						putchar(' ');
-
 					}
-
 				}
-
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
