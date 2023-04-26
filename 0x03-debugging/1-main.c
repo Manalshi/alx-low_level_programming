f60b6c96b877f15ef8dacea3aebf457dd17d27b7
manal <manal.shissah@icloud.com>
2023-04-26 16:13:49 -0700

    gogo amine

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <main.h>
+
 /**
 * main - causes an infinite loop
+*
 * Return: 0
 */
 	int main(void)
@@ -14,6 +17,7 @@
 /*{*/
 /*putchar(i);*/
 /*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
